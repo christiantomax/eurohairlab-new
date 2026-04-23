92554cd0ee2f0a46755658bb68bd816719d8c2e1
Maximillian Christianto <christiantomaximillian@gmail.com>
2026-04-24 03:28:10 +0700

    update metabox

---
+++ wp-content/plugins/meta-box/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wpmetabox/meta-box',
+        'pretty_version' => '5.11.4',
+        'version' => '5.11.4.0',
+        'reference' => 'e704e95b60669ae46e47555278a680096c196077',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'wpmetabox/meta-box' => array(
+            'pretty_version' => '5.11.4',
+            'version' => '5.11.4.0',
+            'reference' => 'e704e95b60669ae46e47555278a680096c196077',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wpmetabox/support' => array(
+            'pretty_version' => '1.0.0',
+            'version' => '1.0.0.0',
+            'reference' => '6c392347cdb77ed133b8a4227b5ee20db89a5717',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wpmetabox/support',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
